fe89babb0ec5b7fe91cd0d4a12c69670b738d2d1
Simul <tsannoyingorange@gmail.com>
2021-03-31 09:07:32 -0500

    better documentation

--- Scripts/TypeWriterEffect.cs
+++ Scripts/TypeWriterEffect.cs
@@ -5,13 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class TypeWriterEffect : MonoBehaviour
 {
-
-	public float delay = 0.1f;
-	public float startDelay = 0f;
-	public bool started;
-	public string fullText;
-	private string currentText = "";
-	public TextMesh tm;
+	public float delay = 0.1f; // this is the delay between checks
+	public float startDelay = 0f; // this is the start delay before it begins
+	private bool started; // hidden variable, stops it from running multiple times over
+	public string fullText; // the full message for it to type out
+	private string currentText = ""; // the current message on screen
+	public TextMesh tm; // this can be either Text or TextMesh based on using default text or textmeshpro
 
 	// Use this for initialization
 	void LateUpdate()
@@ -27,9 +26,9 @@ public class TypeWriterEffect : MonoBehaviour
 
 		for (int i = 0; i <= fullText.Length; i++)
 		{ // the original thing uses < which cuts out the last letter, edited to <= - rusty
-			currentText = fullText.Substring(0, i);// I do not know how this part of the script works at all but it does - rusty
+			currentText = fullText.Substring(0, i);// I now know what this part of the script does hooray
 			tm.text = currentText;
-			yield return new WaitForSeconds(delay);
+			yield return new WaitForSeconds(delay); 
 		}
 	}
 }
